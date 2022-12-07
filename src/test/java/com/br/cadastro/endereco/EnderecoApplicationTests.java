06aaacb3f86b4e192bbeb8b32e0ded818920096d
stephaniesantosss <stephanieps2016@hotmail.com>
2022-12-06 22:47:21 -0300

    setup inicial

---
+++ src/test/java/com/br/cadastro/endereco/EnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cadastro.endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
